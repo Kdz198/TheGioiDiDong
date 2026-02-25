41456f64b37e44c53f1d8c4ba694a24ccd58209c
HoangTuGio <khangdeptrai198@gmail.com>
2026-02-26 03:30:37 +0700

    Refactor Stucture Project for MonoRepo BE FE

---
+++ BackEnd/APIGateway/src/main/java/tgdd/org/apigateway/CorsGlobalConfiguration.java
@@ -0,0 +1,27 @@
+package tgdd.org.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsGlobalConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "https://your-production-fe.com","http://localhost:8080"));
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
