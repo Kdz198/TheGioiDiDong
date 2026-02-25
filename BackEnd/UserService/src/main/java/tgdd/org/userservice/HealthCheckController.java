c87d63baef25c253c02d2534fda096fccbf7c48a
HoangTuGio <khangdeptrai198@gmail.com>
2026-02-26 05:03:05 +0700

    fix APiGateway - springcloud version for use webflux instead mvc

---
+++ BackEnd/UserService/src/main/java/tgdd/org/userservice/HealthCheckController.java
@@ -0,0 +1,24 @@
+package tgdd.org.userservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+
+    @GetMapping("/api/v1/users/health")
+    public String healthCheckV1() {
+        return "User Service is healthy! (v1)";
+    }
+
+    @GetMapping
+    public String welcome() {
+        return "Welcome to User Service!";
+    }
+
+    @GetMapping("health")
+    public String healthCheck() {
+        return "User Service is healthy!";
+        }
+}
