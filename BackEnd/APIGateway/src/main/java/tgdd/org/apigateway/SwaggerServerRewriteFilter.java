cab28da62f3b8135349eb456dc8d409c70272ab7
HoangTuGio <khangdeptrai198@gmail.com>
2026-02-26 22:51:28 +0700

    Update production URL in SwaggerServerRewriteFilter to a valid endpoint

--- BackEnd/APIGateway/src/main/java/tgdd/org/apigateway/SwaggerServerRewriteFilter.java
+++ BackEnd/APIGateway/src/main/java/tgdd/org/apigateway/SwaggerServerRewriteFilter.java
@@ -29,7 +29,7 @@ public class SwaggerServerRewriteFilter implements WebFilter {
     @Value("${app.openapi.dev-url:http://localhost:8090}")
     private String devUrl;
 
-    @Value("${app.openapi.prod-url:Coming_soon}")
+    @Value("${app.openapi.prod-url:http://comingsoon.com}")
     private String prodUrl;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
