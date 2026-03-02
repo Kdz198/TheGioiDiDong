d6813f90f5d4756f58c3b018fdc02e19a6906534
HoangTuGio <khangdeptrai198@gmail.com>
2026-03-02 23:04:30 +0700

    add Order Service configuration, health check, and Docker setup github action for cicd

--- BackEnd/OrderService/src/main/java/fpt/com/orderservice/HealthCheck.java
+++ BackEnd/OrderService/src/main/java/fpt/com/orderservice/HealthCheck.java
@@ -1,20 +1,20 @@
-package tgdd.org.productservice;
+package fpt.com.orderservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/products/health")
-public class HealthCheckController {
+@RequestMapping("/api/orders/health")
+public class HealthCheck {
 
     @GetMapping("health-check")
-    public String healthCheckV1() {
-        return "Product Service is healthy! (v1)";
+    public String healthCheck() {
+        return "Order Service is healthy!";
     }
 
     @GetMapping
     public String welcome() {
-        return "Welcome to Product Service!";
+        return "Welcome to Order Service!";
     }
 }
