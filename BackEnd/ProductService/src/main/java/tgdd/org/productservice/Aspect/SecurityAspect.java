32a05ad9f3d3c549149d1212b5338f347248c649
HoangTuGio <khangdeptrai198@gmail.com>
2026-03-07 14:16:30 +0700

    feat add custom exception handling and security aspect for JWT authentication in ProductService

---
+++ BackEnd/ProductService/src/main/java/tgdd/org/productservice/Aspect/SecurityAspect.java
@@ -0,0 +1,30 @@
+package tgdd.org.productservice.Aspect;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import tgdd.org.productservice.annotation.RequireAuth;
+import tgdd.org.productservice.util.SecurityUtil;
+
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class SecurityAspect {
+
+
+
+    @Before("@annotation(requireAuth)")
+    public void checkSecurity(RequireAuth requireAuth) {
+
+        // 1. Lấy Request hiện tại
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String authHeader = request.getHeader("Authorization");
+
+        SecurityUtil.verifyTokenAndAuth(authHeader, requireAuth.roles(), requireAuth.permission());
+    }
+}
---
+++ BackEnd/ProductService/src/main/java/tgdd/org/productservice/util/SecurityUtil.java
@@ -0,0 +1,64 @@
+package tgdd.org.productservice.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import tgdd.org.productservice.exception.CustomException;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+public class SecurityUtil {
+
+    @Value("${jwt.secret:this_is_fake}")
+    private static String SECRET;
+    private final long EXPIRATION_TIME = 86400000; // 1 ngày
+
+    private static SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static void verifyTokenAndAuth(String authHeader, String[] requiredRoles, String requiredPermission) {
+        // 1. Kiểm tra Header
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new RuntimeException("UNAUTHORIZED - Vui lòng gửi kèm Token hợp lệ!");
+        }
+
+        String token = authHeader.substring(7);
+        Claims claims;
+        try {
+            claims = Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        } catch (JwtException e) {
+            throw new CustomException("UNAUTHORIZED - Token không hợp lệ hoặc đã hết hạn!", HttpStatus.UNAUTHORIZED);
+        }
+
+        // 3. Lấy thông tin từ Payload
+        String tokenRole = claims.get("role", String.class);
+        // JJWT tự động map mảng JSON thành List
+        List<String> tokenPermissions = claims.get("permissions", List.class);
+
+        // 4. Kiểm tra Role (Nếu Annotation có yêu cầu)
+        if (requiredRoles.length > 0) {
+            // Nếu role của user KHÔNG nằm trong mảng requiredRoles -> Chặn
+            if (!Arrays.asList(requiredRoles).contains(tokenRole)) {
+                throw new CustomException("UNAUTHORIZED - Bạn không có quyền truy cập!", HttpStatus.UNAUTHORIZED);
+            }
+        }
+
+        // 5. Kiểm tra Permission (Nếu Annotation có yêu cầu)
+        if (!requiredPermission.isEmpty()) {
+            if (tokenPermissions == null || !tokenPermissions.contains(requiredPermission)) {
+                throw new CustomException("UNAUTHORIZED - Bạn không có quyền truy cập!", HttpStatus.UNAUTHORIZED);
+            }
+        }
+    }
+}
