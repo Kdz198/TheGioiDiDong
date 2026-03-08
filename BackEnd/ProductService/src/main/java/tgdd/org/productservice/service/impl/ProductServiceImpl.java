e39aa351049b561aadf461dde51802820f8c6856
Thu ha <nha3697@gmail.com>
2026-03-08 20:55:54 +0700

    fix import

--- BackEnd/ProductService/src/main/java/tgdd/org/productservice/service/impl/ProductServiceImpl.java
+++ BackEnd/ProductService/src/main/java/tgdd/org/productservice/service/impl/ProductServiceImpl.java
@@ -1,10 +1,11 @@
 package tgdd.org.productservice.service.impl;
 
-import fpt.com.orderservice.exception.CustomException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import tgdd.org.productservice.config.Producer;
+import tgdd.org.productservice.exception.CustomException;
 import tgdd.org.productservice.mapper.ProductMapper;
 import tgdd.org.productservice.model.Brand;
 import tgdd.org.productservice.model.Category;
